ce8d0da167c8118c40c6ebab062c93e5c94d10e3
strikersfran <carrionfn@gmail.com>
2018-08-01 10:23:27 -0400

    Cambiando informacion en la pagina welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,28 +66,15 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FFZERRO1
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">Documentación</a>                    
+                    <a href="https://packagist.org/packages/strikersfran/ffzero1">Packagist</a>
+                    <a href="https://github.com/strikersfran/ffzero1">GitHub</a>
                 </div>
             </div>
         </div>
